62ac4f367564c8cb53a9b38a97277f687f01071f
tom <tom@dnomaid.co.uk>
2017-05-29 06:34:47 +0100

    Add new staff members/compress images

--- index.php
+++ index.php
@@ -22,25 +22,25 @@
         <h2 class="subtitle">Who's in charge?</h2>
         <div class="columns">
             <div class="column">
-                <a href="http://da532.com" target="_blank"><img src="img/people/da532.png" class="someone ceo"></a>
+                <a href="http://da532.com" target="_blank" rel="noopener noreferrer"><img src="img/people/da532.png" class="someone ceo"></a>
                 <h3>Devon (Da532)</h3>
                 <p>Server Owner, CEO</p>
             </div>
 
               <div class="column">
-                <a href="https://youtube.com/FletchXYZ" target="_blank"><img src="img/people/fletch.png" class="someone ceo"></a>
+                <a href="https://youtube.com/FletchXYZ" target="_blank" rel="noopener noreferrer"><img src="img/people/fletch.png" class="someone ceo"></a>
                 <h3>Haden (FletchXYZ)</h3>
                 <p>CEO</p>
             </div>
 
             <div class="column">
-                <a href="http://thunderte.ch" target="_blank"><img src="img/people/thunder.png" class="someone manager"></a>
+                <a href="http://thunderte.ch" target="_blank" rel="noopener noreferrer"><img src="img/people/thunder.png" class="someone manager"></a>
                 <h3>Cameron (Thunder)</h3>
                 <p>Manager</p>
             </div>
 
             <div class="column">
-                <a href="https://dnomaid.co.uk" target="_blank"><img src="img/people/dnomaid.png" class="someone manager"></a>
+                <a href="https://dnomaid.co.uk" target="_blank" rel="noopener noreferrer"><img src="img/people/dnomaid.png" class="someone manager"></a>
                 <h3>Tom (dnomaid)</h3>
                 <p>Manager</p>
             </div>
@@ -48,26 +48,26 @@
         </div>
 
         <div class="columns">
-          <div class="column">
-                <a href="http://www.windowslogic.co.uk/" target="_blank"><img src="img/people/wlp.png" class="someone admin-plus"></a>
+          <!-- <div class="column">
+                <a href="http://www.windowslogic.co.uk/" target="_blank" rel="noopener noreferrer"><img src="img/people/wlp.png" class="someone admin-plus"></a>
                 <h3>Tim <span title="WindowsLogic Productions">(WLP)</a></h3>
                 <p>Head Admin</p>
-          </div>
+          </div> -->
 
             <div class="column">
-                <a href="https://therandommelon.xyz/" target="_blank"><img src="img/people/melon.png" class="someone admin"></a>
+                <a href="https://therandommelon.xyz/" target="_blank" rel="noopener noreferrer"><img src="img/people/melon.png" class="someone admin"></a>
                 <h3>Michael <span title="TheRandomMelon">(TRM)</span></h3>
                 <p>Admin</p>
             </div>
 
             <div class="column">
-                <a href="https://www.discord.io/espislounge" target="_blank"><img src="img/people/espi.png" class="someone admin"></a>
+                <a href="https://www.discord.io/espislounge" target="_blank" rel="noopener noreferrer"><img src="img/people/espi.png" class="someone admin"></a>
                 <h3>Espi Netsky</h3>
                 <p>Admin</p>
             </div>
 
             <div class="column">
-                <a href="https://crashieblog.wordpress.com" target="_blank"><img src="img/people/crashie.png" class="someone admin"></a>
+                <a href="https://crashieblog.wordpress.com" target="_blank" rel="noopener noreferrer"><img src="img/people/crashie.png" class="someone admin"></a>
                 <h3>Kira (Crashie)</h3>
                 <p>Admin</p>
             </div>
@@ -88,7 +88,7 @@
           </div>
 
           <div class="column">
-              <a href="https://www.youtube.com/channel/UC04awUMPfLwEGtCCWvYq-UQ" target="_blank"><img src="img/people/anidox.png" class="someone mod"></a>
+              <a href="https://www.youtube.com/channel/UC04awUMPfLwEGtCCWvYq-UQ" target="_blank" rel="noopener noreferrer"><img src="img/people/anidox.png" class="someone mod"></a>
               <h3>Anidox</h3>
               <p>Mod</p>
           </div>
@@ -102,6 +102,19 @@
           <div class="column">
               <img src="img/people/rainbow.png" class="someone mod">
               <h3>Nicole <span title="RainbowStar331">(RS331)</span></h3>
+              <p>Mod</p>
+          </div>
+
+          <div class="column">
+              <a href="https://www.youtube.com/channel/UCfpY45UhtJ9i-_PJVwBOzvA
+" target="_blank" rel="noopener noreferrer"><img src="img/people/jaffa.png" class="someone mod"></a>
+              <h3 title="AKA TheJaffaMeme">TheJaffaMan</h3>
+              <p>Mod</p>
+          </div>
+
+          <div class="column">
+              <a href="https://bobpony.org/" target="_blank" rel="noopener noreferrer"><img src="img/people/bobpony.png" class="someone mod"></a>
+              <h3>Bob Pony</h3>
               <p>Mod</p>
           </div>
 
