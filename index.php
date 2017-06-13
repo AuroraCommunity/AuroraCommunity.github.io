441eecf69d528383774585ad67694b2f3ef8037b
Thunder Tech <thunderwavepony@outlook.com>
2017-06-13 10:09:17 +0100

    Update index.php
    Updated a few things.

--- index.php
+++ index.php
@@ -7,42 +7,48 @@
   <?php include 'include/hero.php';?>
 
     <div id="content">
-      <div id="content-inner">
+      
 
-    <div id="about" class="section" style="text-align: center;">
+    <div id="about" class="box" style="text-align: center;">
         <h1 class="title"><strong>About</strong></h1>
         <h2 class="subtitle">What even is Aurora?</h2>
         <p>Aurora is a community of people.</p>
         <p>Since late 2016 we've amassed over 2000 members.</p>
         <p>We're mostly interested in technology and outing tech support scammers.</p>
     </div>
-
-    <div id="staff" class="section" style="text-align: center;">
+			<div id="content-inner">
+    <div id="staff" class="box" style="text-align: center;">
         <h1 class="title"><strong>Staff</strong></h1>
         <h2 class="subtitle">Who's in charge?</h2>
         <div class="columns" id="managers">
             <div class="column">
                 <a href="http://da532.com" target="_blank" rel="noopener noreferrer"><img src="img/people/da532.png" class="someone ceo"></a>
                 <h3>Devon (Da532)</h3>
-                <p>Server Owner, CEO</p>
+                <span class="tag is-danger">Owner & CEO</span>
             </div>
 
             <div class="column">
                 <a href="https://www.scammer.info" target="_blank" rel="noopener noreferrer"><img src="img/people/thunder.png" class="someone ceo"></a>
-                <h3>Kevin (Thunder)</h3>
-                <p>CEO</p>
+                <h3>Thunder</h3>
+                <span class="tag is-danger">CEO</span>
             </div>
 
             <div class="column">
                 <a href="https://dnomaid.co.uk" target="_blank" rel="noopener noreferrer"><img src="img/people/dnomaid.png" class="someone manager"></a>
-                <h3>Tom (dnomaid)</h3>
-                <p>Manager</p>
+                <h3>Dnomaid</h3>
+                <span class="tag is-danger">Manager</span>
+            </div>
+			
+			<div class="column">
+                <img src="img/people/fletch.png" class="someone manager"></a>
+                <h3>Haden</h3>
+                <span class="tag is-danger">Manager</span>
             </div>
 
             <div class="column">
                 <a href="http://www.ao554.ml/" target="_blank" rel="noopener noreferrer"><img src="img/people/ao.png" class="someone manager"></a>
-                <h3>Harrison (AO554)</h3>
-                <p>Manager</p>
+                <h3>Harrison</h3>
+                <span class="tag is-danger">Manager</span>
             </div>
 
         </div>
@@ -51,38 +57,38 @@
           <div class="column">
                 <a href="https://www.youtube.com/channel/UCyOFNvNzwQIC3SL-MksEnbw/" target="_blank" rel="noopener noreferrer"><img src="img/people/regan.png" class="someone admin-plus"></a>
                 <h3>Aoelus <span title="Aoelus">(Regan)</a></h3>
-                <p>Head Admin</p>
+                <span class="tag is-info">Head Admin</span>
           </div>
 
             <div class="column">
                 <a href="https://therandommelon.xyz/" target="_blank" rel="noopener noreferrer"><img src="img/people/melon.png" class="someone admin"></a>
                 <h3>Michael <span title="TheRandomMelon">(TRM)</span></h3>
-                <p>Admin</p>
+                <span class="tag is-info">Admin</span>
             </div>
 
             <div class="column">
                 <a href="https://www.youtube.com/channel/UCmLtdx7F8QFnsWYUfDSPeKg/" target="_blank" rel="noopener noreferrer"><img src="img/people/technach.png" class="someone admin"></a>
                 <h3>Technach <span title="Technach">(Josh)</a></h3>
-                <p>Admin</p>
+                <span class="tag is-info">Admin</span>
             </div>
 
             <div class="column">
                 <a href="https://crashieblog.wordpress.com" target="_blank" rel="noopener noreferrer"><img src="img/people/crashie.png" class="someone admin"></a>
                 <h3>Kira (Crashie)</h3>
-                <p>Admin</p>
+                <span class="tag is-info">Admin</span>
             </div>
 
 
             <div class="column">
                 <a href="https://www.youtube.com/channel/UC04awUMPfLwEGtCCWvYq-UQ" target="_blank" rel="noopener noreferrer"><img src="img/people/anidox.png" class="someone admin"></a>
                 <h3>Anidox</h3>
-                <p>Admin</p>
+                <span class="tag is-info">Admin</span>
           </div>
 		  
 		    <div class="column">
                 <img src="img/people/joe.png" class="someone admin"></a>
                 <h3>Joe <span title="Joe">(Flich)</a></h3>
-                <p>Admin</p>
+                <span class="tag is-info">Admin</span>
           </div>
 
         </div>
@@ -92,32 +98,32 @@
           <div class="column">
               <img src="img/people/torazer.png" class="someone mod">
               <h3>Torazer</h3>
-              <p>Mod</p>
+              <span class="tag is-warning">Moderator</span>
           </div>
 
           <div class="column">
               <img src="img/people/duke.png" class="someone mod">
               <h3>Dukiverse</h3>
-              <p>Mod</p>
+              <span class="tag is-warning">Moderator</span>
           </div>
 
           <div class="column">
               <a href="https://www.youtube.com/channel/UCfpY45UhtJ9i-_PJVwBOzvA
 " target="_blank" rel="noopener noreferrer"><img src="img/people/jaffa.png" class="someone mod"></a>
               <h3><span title="AKA TheJaffaMeme">TheJaffaMan</span></h3>
-              <p>Mod</p>
+              <span class="tag is-warning">Moderator</span>
           </div>
 
           <div class="column">
             <img src="img/people/ren.png" class="someone mod">
             <h3>Renekton</h3>
-            <p>Mod</p>
+            <span class="tag is-warning">Moderator</span>
         </div>
 
         <div class="column">
           <img src="img/people/trashy.png" class="someone mod">
           <h3>TrashyClassy</h3>
-          <p>Mod</p>
+          <span class="tag is-warning">Moderator</span>
       </div>
 
     </div>
